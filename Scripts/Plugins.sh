93c4c1552616dd58f0071802ee2e7bf6aa0ab3be
Rolay <rolay8@gmail.com>
2026-04-04 22:16:24 +0800

    Clean up environment variable exports in Plugins.sh
    Remove unused CORE_VER and TUN_VER exports from Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -21,11 +21,8 @@ git clone --depth=1 --single-branch https://github.com/gdy666/luci-app-lucky.git
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 
 #预置OpenClash内核和GEO数据
-export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/refs/heads/core/dev/core_version
 export CORE_MATE=https://github.com/vernesong/OpenClash/raw/refs/heads/core/dev/meta/clash-linux
-
 export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
 
 export GEO_MMDB=https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
 export GEO_SITE=https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
