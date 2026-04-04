6c49cd55ea96c50bc84750e9428bf22e8389c249
Rolay <rolay8@gmail.com>
2026-04-04 22:14:49 +0800

    Fix URLs for core version and meta in Plugins.sh
    Updated URLs for core version and meta files in the OpenClash script.

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -21,10 +21,8 @@ git clone --depth=1 --single-branch https://github.com/gdy666/luci-app-lucky.git
 git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/OpenClash.git
 
 #预置OpenClash内核和GEO数据
-export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
-export CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux
-export CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux
-export CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux
+export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/refs/heads/core/dev/core_version
+export CORE_MATE=https://github.com/vernesong/OpenClash/raw/refs/heads/core/dev/meta/clash-linux
 
 export CORE_TYPE=$(echo $OWRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
 export TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
@@ -41,13 +39,7 @@ curl -sfL -o ./GeoIP.dat $GEO_IP
 
 mkdir ./core && cd ./core
 
-curl -sfL -o ./tun.gz "$CORE_TUN"-"$CORE_TYPE"-"$TUN_VER".gz
-gzip -d ./tun.gz && mv ./tun ./clash_tun
-
 curl -sfL -o ./meta.tar.gz "$CORE_MATE"-"$CORE_TYPE".tar.gz
 tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
-
-curl -sfL -o ./dev.tar.gz "$CORE_DEV"-"$CORE_TYPE".tar.gz
-tar -zxf ./dev.tar.gz
 
 chmod +x ./clash* ; rm -rf ./*.gz
